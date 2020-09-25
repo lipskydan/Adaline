029ef460051091678950736c9acdaf9cc9201c6f
lipskydan <lipsky@knu.ua>
2020-09-25 12:15:42 +0300

    adaline gradient descent [standardized]

--- App/main.py
+++ App/main.py
@@ -7,6 +7,8 @@ from matplotlib.colors import ListedColormap
 class AdalineGD(object):
     """Adaptive Linear Neuron classifier.
 
+    GD - Gradient Descent
+
     Parameters
     ------------
     eta : float
@@ -74,6 +76,35 @@ class AdalineGD(object):
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
 
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+
 v1 = np.array([1, 2, 3])
 v2 = 0.5 * v1
 np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
@@ -91,8 +122,6 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 # extract sepal length and petal length
 X = df.iloc[0:100, [0, 2]].values
 
-
-
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
 ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
@@ -107,5 +136,28 @@ ax[1].set_xlabel('Эпохи')
 ax[1].set_ylabel('Сумма квадратичных ошибок')
 ax[1].set_title('Adaline - Скорость обучения 0.0001')
 
-plt.savefig('images/learning_rate.png', dpi=300)
+plt.savefig('images/learning_rate_AdalineGD.png', dpi=300)
+plt.show()
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - градиентный спуск')
+plt.xlabel('длина чашелистка [стандартизированная]')
+plt.ylabel('длина лепестка [стандартизированная]')
+plt.legend(loc='upper left')
+
+plt.savefig('images/adaline_gradient_descent', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Эпохи')
+plt.ylabel('Сумма квадратичных ошибок')
+
+plt.savefig('images/learning_rate_AdalineGD_standardized.png', dpi=300)
 plt.show()
