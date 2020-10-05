50e1d736e653d94de79881064a725ce43eae1a4f
lipskydan <lipsky@knu.ua>
2020-10-05 12:18:56 +0300

    add comments to class AdalineSGD

--- App/adalineSGD.py
+++ App/adalineSGD.py
@@ -5,6 +5,32 @@ from visualizer import plot_decision_regions
 
 
 class AdalineSGD(object):
+    """Adaptive Linear Neuron classifier.
+
+        SGD - Stochastic Gradient Descent
+
+        Parameters
+        ------------
+        eta : float
+          Learning rate (between 0.0 and 1.0)
+        n_iter : int
+          Passes over the training dataset.
+        shuffle : bool (default: True)
+          Shuffles training data every epoch if True to prevent cycles.
+        random_state : int
+          Random number generator seed for random weight
+          initialization.
+
+
+        Attributes
+        -----------
+        w_ : 1d-array
+          Weights after fitting.
+        cost_ : list
+          Sum-of-squares cost function value averaged over all
+          training samples in each epoch.
+        """
+
     def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
         self.eta = eta
         self.n_iter = n_iter
