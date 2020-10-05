e36d64b045b2953a1d97bf239fa988afef2c5ae2
lipskydan <lipsky@knu.ua>
2020-10-05 12:12:04 +0300

    moved def plot_decision_regions to own file.py

--- App/adalineGD.py
+++ App/adalineGD.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+from visualizer import plot_decision_regions
 
 
 class AdalineGD(object):
@@ -76,33 +76,33 @@ class AdalineGD(object):
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
 
 
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-                    y=X[y == cl, 1],
-                    alpha=0.8,
-                    c=colors[idx],
-                    marker=markers[idx],
-                    label=cl,
-                    edgecolor='black')
+# def plot_decision_regions(X, y, classifier, resolution=0.02):
+#
+#     # setup marker generator and color map
+#     markers = ('s', 'x', 'o', '^', 'v')
+#     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+#     cmap = ListedColormap(colors[:len(np.unique(y))])
+#
+#     # plot the decision surface
+#     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+#     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+#     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+#                            np.arange(x2_min, x2_max, resolution))
+#     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+#     Z = Z.reshape(xx1.shape)
+#     plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+#     plt.xlim(xx1.min(), xx1.max())
+#     plt.ylim(xx2.min(), xx2.max())
+#
+#     # plot class samples
+#     for idx, cl in enumerate(np.unique(y)):
+#         plt.scatter(x=X[y == cl, 0],
+#                     y=X[y == cl, 1],
+#                     alpha=0.8,
+#                     c=colors[idx],
+#                     marker=markers[idx],
+#                     label=cl,
+#                     edgecolor='black')
 
 
 v1 = np.array([1, 2, 3])
